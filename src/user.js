07530e21f63dd80c68d04d7e963b35a2181a846b
torsami <torsami77@gmail.com>
2019-03-05 06:39:21 +0100

    credentials

--- src/user.js
+++ src/user.js
@@ -13,11 +13,20 @@ app.use(bodyParser.json({ type: 'application/json'}));
 const secret = 'secret';
 
 const pool = new pg.Pool({
-    user: 'samipostgres',
+   /* user: 'samipostgres',
     host: '127.0.0.1',
     database: 'mydatabase',
     password: 'samipostgres',
-    port: '5432'});
+    port: '5432'
+    */
+
+    //heroku
+    host: 'ec2-54-225-237-84.compute-1.amazonaws.com',
+    Database: 'd6npb1cqus4ps1',
+    User: 'dcdlhhqbgpkvxx',
+    Port: '5432',
+    Password: 'dfd71496b432bd5b245cc13ce7e57e1a7dc81202e7ea862919a3a86ca24d2ed2'
+});
 
 
 const verifyToken = (req, res, next)=>{
