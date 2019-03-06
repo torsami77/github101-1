835b1f605745e97a3a62419f6e9ad90c018b00ec
torsami <torsami77@gmail.com>
2019-03-06 14:03:19 +0100

    poolconnect

--- src/user.js
+++ src/user.js
@@ -11,22 +11,20 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json'})); 
 
 const secret = 'secret';
-
+/*
 const pool = new pg.Pool({
-   /* user: 'samipostgres',
+    user: 'samipostgres',
     host: '127.0.0.1',
     database: 'mydatabase',
     password: 'samipostgres',
     port: '5432'
-    */
-
-    //heroku
-    Host: 'ec2-107-20-167-11.compute-1.amazonaws.com',
-    Database: 'da9ssf8703fkcj',
-    User: 'fgxmlcmztjlnqa',
-    Port: '5432',
-    Password: '78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645'
 });
+*/
+const connectionString = 'postgres://fgxmlcmztjlnqa:78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645@ec2-107-20-167-11.compute-1.amazonaws.com:5432/da9ssf8703fkcj';
+
+const pool = new Pool({
+  connectionString: connectionString
+})
 
 
 const verifyToken = (req, res, next)=>{
