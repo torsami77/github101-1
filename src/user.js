b6e19e324a8c52419f6ce5c0f110058044c3d066
torsami <torsami77@gmail.com>
2019-03-08 00:52:03 +0100

    eventfull-fixeded

--- src/user.js
+++ src/user.js
@@ -14,15 +14,15 @@ const secret = 'secret';
 const connectionString = 'postgres://fgxmlcmztjlnqa:78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645@ec2-107-20-167-11.compute-1.amazonaws.com:5432/da9ssf8703fkcj';
 
 const pool = new pg.Pool({
-
+/*
     user: 'samipostgres',
     host: '127.0.0.1',
     database: 'mydatabase',
     password: 'samipostgres',
     port: '5432'
+*/
 
-
-//connectionString: 'postgres://fgxmlcmztjlnqa:78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645@ec2-107-20-167-11.compute-1.amazonaws.com:5432/da9ssf8703fkcj'
+connectionString: 'postgres://fgxmlcmztjlnqa:78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645@ec2-107-20-167-11.compute-1.amazonaws.com:5432/da9ssf8703fkcj'
 
 });
 
