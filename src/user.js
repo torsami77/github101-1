878c7616d6a6f62fe781320227dd2437be63b450
torsami <torsami77@gmail.com>
2019-03-29 13:44:35 +0100

    fixed querry

--- src/user.js
+++ src/user.js
@@ -14,11 +14,10 @@ const secret = 'secret';
 const connectionString = 'postgres://fgxmlcmztjlnqa:78ed6f0c9e151eb1f3fbb6e8a7a50ba3d4a9bf471e6498ea527119a2169ea645@ec2-107-20-167-11.compute-1.amazonaws.com:5432/da9ssf8703fkcj';
 
 const pool = new pg.Pool({
-   /*
-    password: '',
-    user: 'postgres',
-    database: 'postgres',
-    */
+   
+   // password: '',
+   // user: 'postgres',
+   // database: 'postgres',
     
     user: 'samipostgres',
     password: 'samipostgres',
